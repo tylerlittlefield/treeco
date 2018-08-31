1cb0809a7a91cb37dc49240baa9ace58cf5054e3
tylurp <tylurp1@gmail.com>
2018-08-31 14:45:10 -0700

    Trying to split eco_run function into smaller functions

--- R/eco_run_all.R
+++ R/eco_run_all.R
@@ -2,19 +2,116 @@
 # Interpolation function
 #-------------------------------------------------------------------------------
 eco_interp <- function(x, x1, y1, x2, y2) {
-
   y = ((x - x1) * (y2 - y1) / (x2 - x1)) + y1
   return(y)
-
 }
 
 #-------------------------------------------------------------------------------
 # String similarity function
 #-------------------------------------------------------------------------------
 string_dist <- function(str_1, str_2) {
-
   1 - (utils::adist(str_1, str_2) / pmax(nchar(str_1), nchar(str_2)))
+}
 
+# ------------------------------------------------------------------------------
+# Data extract function
+# ------------------------------------------------------------------------------
+
+extract <- function(data, species_col, dbh_col, region) {
+  trees    <- data.table::fread(data)
+  benefits <- data.table::as.data.table(treeco::benefits)
+  species  <- data.table::as.data.table(treeco::species)
+  money    <- data.table::as.data.table(treeco::money)
+  money    <- money[money$region_code == region, ]                               # Filter currency dataset by region
+  money    <- data.table::melt(money, id.vars = c("region_code", "region_name")) # Melt the dataset to 'tidy' format
+  money    <- money[, c("variable", "value")]                                    # Select the variables we need
+
+  data.table::setnames(trees, species_col, "common_name")                        # Rename species column to stay consistent
+  data.table::setnames(trees, dbh_col, "dbh_val")                                # Rename dbh column to stay consistent
+
+  trees         <- trees[, .SD, .SDcol = c("common_name", "dbh_val")][trees$dbh_val > 0] # Arrange columns consistently, then extract all records where dbh > 0
+  benefits      <- benefits[grepl(region, species_region)]                            # Extract benefits within user defined region
+  species       <- species[grepl(region, species_region)]                              # Extract species within user defined region
+  trees$dbh_val <- trees$dbh_val * 2.54
+
+  output   <- list(trees = trees,
+                   benefits = benefits,
+                   species = species,
+                   money = money)
+
+  return(output)
+}
+
+#-------------------------------------------------------------------------------
+# Money extract function
+#-------------------------------------------------------------------------------
+
+extract_money <- function(tree_data, money_data) {
+  # A bunch of copy/paste (yuck) to grab eco benefit money values so we can
+  # multiply the benefit by these values to get $ saved. Figure out a more
+  # elegant way of doing this. Looks ugly.
+  elec_money <- money_data[grepl("electricity", money_data$variable)][["value"]]
+  gas_money  <- money_data[grepl("natural_gas", money_data$variable)][["value"]]
+  h20_money  <- money_data[grepl("h20_gal", money_data$variable)][["value"]]
+  co2_money  <- money_data[grepl("co2", money_data$variable)][["value"]]
+  o3_money   <- money_data[grepl("o3_lb", money_data$variable)][["value"]]
+  nox_money  <- money_data[grepl("nox_lb", money_data$variable)][["value"]]
+  pm10_money <- money_data[grepl("pm10_lb", money_data$variable)][["value"]]
+  sox_money  <- money_data[grepl("sox_lb", money_data$variable)][["value"]]
+  voc_money  <- money_data[grepl("voc_lb", money_data$variable)][["value"]]
+
+  # Convert kilograms and cubic meters to lbs and gallons
+  tree_data[grepl("lb", unit), "benefit_value" := benefit_value * 2.20462]
+  tree_data[grepl("gal", unit), "benefit_value" := benefit_value * 264.172052]
+
+  # Multiply $ values by the benefit value to get the $ saved
+  tree_data[grepl("electricity", benefit), "dollars" := benefit_value * elec_money]
+  tree_data[grepl("natural gas", benefit), "dollars" := benefit_value * gas_money]
+  tree_data[grepl("hydro interception", benefit), "dollars" := benefit_value * h20_money]
+  tree_data[grepl("co2 ", benefit), "dollars" := benefit_value * co2_money]
+  tree_data[grepl("aq ozone dep", benefit), "dollars" := benefit_value * o3_money]
+  tree_data[grepl("aq nox", benefit), "dollars" := benefit_value * nox_money]
+  tree_data[grepl("aq pm10", benefit), "dollars" := benefit_value * pm10_money]
+  tree_data[grepl("aq sox", benefit), "dollars" := benefit_value * sox_money]
+  tree_data[grepl("voc", benefit), "dollars" := benefit_value * voc_money]
+
+  # Because davey takes $ values like -0.54 from natural gas and makes it 0.54?
+  tree_data$dollars <- abs(round(tree_data$dollars, 2))
+  tree_data$benefit_value <- round(tree_data$benefit_value, 4)
+  tree_data <- data.table::as.data.table(tree_data)
+}
+
+#-------------------------------------------------------------------------------
+# Parameter extract function (for interpolation equation)
+#-------------------------------------------------------------------------------
+
+extract_parameters <- function(tree_data) {
+  dbh_values <- tree_data[["dbh_val"]]
+  dbh_ranges <- c(3.81, 11.43, 22.86, 38.10, 53.34, 68.58, 83.82, 99.06, 114.30)
+
+  z <- abs(outer(dbh_values, dbh_ranges, `-`))
+
+  tree_data$a <- apply(z, 1, which.min)
+  z[z==apply(z, 1, min)] <- Inf
+  tree_data$b <- apply(z, 1, which.min)
+  tree_data$start <- pmin(tree_data$a, tree_data$b)
+  tree_data$end <- pmax(tree_data$a, tree_data$b)
+  tree_data$x1 <- dbh_ranges[tree_data$start]
+  tree_data$x2 <- dbh_ranges[tree_data$end]
+
+  # Grab the benefit values given the start/end vectors
+  # https://stackoverflow.com/questions/20617371/
+  tbl_rows <- seq_along(tree_data$id)
+  tbl_indicies_y1 <- tree_data$start
+  tbl_mat <- as.matrix(tree_data[,7:15]) # careful with this
+  tree_data$y1 <- tbl_mat[cbind(tbl_rows, tbl_indicies_y1)]
+
+  tbl_rows <- seq_along(tree_data$id)
+  tbl_indicies_y2 <- tree_data$end
+  tbl_mat <- as.matrix(tree_data[,7:15]) # careful with this, easy to fuck up
+  tree_data$y2 <- tbl_mat[cbind(tbl_rows, tbl_indicies_y2)]
+
+  return(tree_data)
 }
 
 #-------------------------------------------------------------------------------
@@ -31,31 +128,15 @@ string_dist <- function(str_1, str_2) {
 #' @import data.table
 #' @export
 eco_run_all <- function(data, species_col, dbh_col, region) {
-
   start_time <- Sys.time()
 
   message("Importing data...")
 
-  # Import trees, benefits, money, and species data
-  trees    <- data.table::fread(data)
-  benefits <- data.table::as.data.table(treeco::benefits)
-  species  <- data.table::as.data.table(treeco::species)
-  money    <- data.table::as.data.table(treeco::money)
-  money    <- money[money$region_code == region, ]                               # Filter currency dataset by region
-  money    <- data.table::melt(money, id.vars = c("region_code", "region_name")) # Melt the dataset to 'tidy' format
-  money    <- money[, c("variable", "value")]                                    # Select the variables we need
-
-  message("Cleaning data...")
-
-  # Clean data
-  data.table::setnames(trees, species_col, "common_name")                        # Rename species column to stay consistent
-  data.table::setnames(trees, dbh_col, "dbh_val")                                # Rename dbh column to stay consistent
-  trees <- trees[, .SD, .SDcol = c("common_name", "dbh_val")][trees$dbh_val > 0] # Arrange columns consistently, then extract all records where dbh > 0
-  benefits <- benefits[grepl(region, species_region)]                            # Extract benefits within user defined region
-  species <- species[grepl(region, species_region)]                              # Extract species within user defined region
-
-  trees$dbh_val <- trees$dbh_val * 2.54 # Convert inches to centimeters
-                                        # TODO: Add an ifelse in case user records dbh in centimeters
+  tree_data <- extract(data, species_col, dbh_col, region)
+  trees     <- tree_data$trees
+  benefits  <- tree_data$benefits
+  species   <- tree_data$species
+  money     <- tree_data$money
 
   # Extract unique records by common_name/dbh_val pairs, we want to run the
   # benefits on the minimum number of records possible and then join to the non
@@ -104,6 +185,7 @@ eco_run_all <- function(data, species_col, dbh_col, region) {
   trees <- trees[trees$common_name %in% unique_common_names$common_name]
   trees_unique <- trees_unique[trees_unique$common_name %in% unique_common_names$common_name]
 
+
   # Set the keys to prepare for the join
   data.table::setkey(unique_common_names, "common_name")
   data.table::setkey(trees_unique, "common_name")
@@ -125,39 +207,7 @@ eco_run_all <- function(data, species_col, dbh_col, region) {
 
   message("Calculating eco benefits...")
 
-  dbh_values <- trees_unique[["dbh_val"]]
-  dbh_ranges <- c(3.81, 11.43, 22.86, 38.10, 53.34, 68.58, 83.82, 99.06, 114.30)
-
-  z <- abs(outer(dbh_values, dbh_ranges, `-`))
-
-  # A function that returns the position of n-th largest
-  # https://stackoverflow.com/questions/10296866/
-  # The commented out stuff is a little faster but needs more testing.
-  # minn <- function(n) function(x) order(x, decreasing = FALSE)[n]
-  # minnp <- function(n) function(x) sort(x, partial = 1)[n]
-  trees_unique$a <- apply(z, 1, which.min)
-  z[z==apply(z, 1, min)] <- Inf
-  trees_unique$b <- apply(z, 1, which.min)
-  # message("Creating a var...")
-  # trees_unique$a <- apply(z, 1, minn(1))
-  # message("Creating b var...")
-  # trees_unique$b <- apply(z, 1, minn(2))
-  trees_unique$start <- pmin(trees_unique$a, trees_unique$b)
-  trees_unique$end <- pmax(trees_unique$a, trees_unique$b)
-  trees_unique$x1 <- dbh_ranges[trees_unique$start]
-  trees_unique$x2 <- dbh_ranges[trees_unique$end]
-
-  # Grab the benefit values given the start/end vectors
-  # https://stackoverflow.com/questions/20617371/
-  tbl_rows <- seq_along(trees_unique$id)
-  tbl_indicies_y1 <- trees_unique$start
-  tbl_mat <- as.matrix(trees_unique[,7:15]) # careful with this
-  trees_unique$y1 <- tbl_mat[cbind(tbl_rows, tbl_indicies_y1)]
-
-  tbl_rows <- seq_along(trees_unique$id)
-  tbl_indicies_y2 <- trees_unique$end
-  tbl_mat <- as.matrix(trees_unique[,7:15]) # careful with this, easy to fuck up
-  trees_unique$y2 <- tbl_mat[cbind(tbl_rows, tbl_indicies_y2)]
+  trees_unique <- extract_parameters(trees_unique)
 
   tree_vars <- c("id", "scientific_name", "common_name", "dbh_val", "x1", "x2", "y1", "y2", "benefit", "unit")
   trees_unique <- trees_unique[, .SD, .SDcols = tree_vars]
@@ -167,9 +217,6 @@ eco_run_all <- function(data, species_col, dbh_col, region) {
 
   message("Calculating $ benefits...")
 
-  tree_vars <- c("scientific_name", "common_name", "dbh_val", "benefit_value", "benefit", "unit", "x1", "x2", "y1", "y2")
-  trees_unique <- trees_unique[, .SD, .SDcols = tree_vars]
-
   tree_vars <- c("id", "common_name", "dbh_val", "benefit")
   trees <- trees[, .SD, .SDcols = tree_vars]
 
@@ -177,37 +224,7 @@ eco_run_all <- function(data, species_col, dbh_col, region) {
   trees_unique <- stats::na.omit(trees_unique)
   trees <- stats::na.omit(trees)
 
-  # A bunch of copy/paste (yuck) to grab eco benefit money values so we can
-  # multiply the benefit by these values to get $ saved. Figure out a more
-  # elegant way of doing this. Looks ugly.
-  elec_money <- money[grepl("electricity", money$variable)][["value"]]
-  gas_money  <- money[grepl("natural_gas", money$variable)][["value"]]
-  h20_money  <- money[grepl("h20_gal", money$variable)][["value"]]
-  co2_money  <- money[grepl("co2", money$variable)][["value"]]
-  o3_money   <- money[grepl("o3_lb", money$variable)][["value"]]
-  nox_money  <- money[grepl("nox_lb", money$variable)][["value"]]
-  pm10_money <- money[grepl("pm10_lb", money$variable)][["value"]]
-  sox_money  <- money[grepl("sox_lb", money$variable)][["value"]]
-  voc_money  <- money[grepl("voc_lb", money$variable)][["value"]]
-
-  # Convert kilograms and cubic meters to lbs and gallons
-  trees_unique[grepl("lb", unit), "benefit_value" := benefit_value * 2.20462]
-  trees_unique[grepl("gal", unit), "benefit_value" := benefit_value * 264.172052]
-
-  # Multiply $ values by the benefit value to get the $ saved
-  trees_unique[grepl("electricity", benefit), "dollars" := benefit_value * elec_money]
-  trees_unique[grepl("natural gas", benefit), "dollars" := benefit_value * gas_money]
-  trees_unique[grepl("hydro interception", benefit), "dollars" := benefit_value * h20_money]
-  trees_unique[grepl("co2 ", benefit), "dollars" := benefit_value * co2_money]
-  trees_unique[grepl("aq ozone dep", benefit), "dollars" := benefit_value * o3_money]
-  trees_unique[grepl("aq nox", benefit), "dollars" := benefit_value * nox_money]
-  trees_unique[grepl("aq pm10", benefit), "dollars" := benefit_value * pm10_money]
-  trees_unique[grepl("aq sox", benefit), "dollars" := benefit_value * sox_money]
-  trees_unique[grepl("voc", benefit), "dollars" := benefit_value * voc_money]
-
-  # Because davey takes $ values like -0.54 from natural gas and makes it 0.54?
-  trees_unique$dollars <- abs(round(trees_unique$dollars, 2))
-  trees_unique$benefit_value <- round(trees_unique$benefit_value, 4)
+  trees_unique <- extract_money(trees_unique, money)
 
   data.table::setkey(trees, NULL)
   data.table::setkey(trees_unique, NULL)
@@ -234,5 +251,4 @@ eco_run_all <- function(data, species_col, dbh_col, region) {
   print(elapsed_time)
 
   return(trees_final)
-
 }
