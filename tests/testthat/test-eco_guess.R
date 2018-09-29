e167de0146e47899eaad3a9af76c371773d135c3
tylurp <tylurp1@gmail.com>
2018-09-29 12:17:08 -0700

    Added two test for the things just added to eco_guess: converting factor to character and stopping if guess arg isn't 'botanical' or 'common'.

--- tests/testthat/test-eco_guess.R
+++ tests/testthat/test-eco_guess.R
@@ -1,6 +1,16 @@
 context("test-eco_guess")
 
 test_that("eco_guess works", {
+
+  # Test that eco guess can guess the botanical names
   expect_equal(eco_guess("common fig", "botanical"), "Ficus carica")
+
+  # Test that eco guess can guess the common names
   expect_equal(eco_guess("pinus elderica", "common"), "Afghan pine")
+
+  # Test that eco guess converts factor to character
+  expect_equal(is.character(eco_guess(as.factor("Common fig"), "botanical")), TRUE)
+
+  # Test that eco guess throws an error if guess arg isn't 'common' or 'botanical'
+  expect_error(eco_guess("common fig", "botancal"), "Guess arg isn't 'common' or 'botanical'.")
 })
