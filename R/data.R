6863a32d17807c82e8ce7af551a6631e47366d15
tylurp <tylurp1@gmail.com>
2018-10-18 13:38:11 -0700

    minor data documentation changes

--- R/data.R
+++ R/data.R
@@ -6,20 +6,20 @@
 #'
 #' @format A data frame with 6975 rows and 14 variables:
 #' \describe{
-#'   \item{id}{id variable.}
+#'   \item{id}{An id variable.}
 #'   \item{species_code}{This is the "spp_value_assignment" and acts as a key linking trees to their appropriate benefit values.}
-#'   \item{x3_81}{DBH range of 3.81 centimeters.}
-#'   \item{x11_43}{DBH range of 11.43 centimeters.}
-#'   \item{x22_86}{DBH range of 22.86 centimeters.}
-#'   \item{x38_10}{DBH range of 38.10 centimeters.}
-#'   \item{x53_34}{DBH range of 53.34 centimeters.}
-#'   \item{x68_58}{DBH range of 68.58 centimeters.}
-#'   \item{x83_82}{DBH range of 83.82 centimeters.}
-#'   \item{x99_06}{DBH range of 99.06 centimeters.}
-#'   \item{x114_30}{DBH range of 114.30 centimeters.}
+#'   \item{x3_81}{Benefit values associated with a DBH range of 3.81 centimeters.}
+#'   \item{x11_43}{Benefit values associated with a DBH range of 11.43 centimeters.}
+#'   \item{x22_86}{Benefit values associated with a DBH range of 22.86 centimeters.}
+#'   \item{x38_10}{Benefit values associated with a DBH range of 38.10 centimeters.}
+#'   \item{x53_34}{Benefit values associated with a DBH range of 53.34 centimeters.}
+#'   \item{x68_58}{Benefit values associated with a DBH range of 68.58 centimeters.}
+#'   \item{x83_82}{Benefit values associated with a DBH range of 83.82 centimeters.}
+#'   \item{x99_06}{Benefit values associated with a DBH range of 99.06 centimeters.}
+#'   \item{x114_30}{Benefit values associated with a DBH range of 114.30 centimeters.}
 #'   \item{species_region}{The region code representing a specific region. Benefit values change depending on the region.}
 #'   \item{benefit}{The type of benefit.}
-#'   \item{unit}{The unit of measurment for a benefit.}
+#'   \item{unit}{The unit of measurement for a benefit type.}
 #'   ...
 #' }
 #' @source \url{https://www.itreetools.org}
@@ -31,18 +31,18 @@
 #'
 #' @format A data frame with 3178 rows and 12 variables:
 #' \describe{
-#'   \item{id}{id variable.}
-#'   \item{species_code}{species code}
-#'   \item{scientific_name}{scientific name of tree}
-#'   \item{common_name}{common name of tree}
-#'   \item{tree_type}{type of tree}
-#'   \item{spp_value_assignment}{another tree code for linking trees to benefits}
+#'   \item{id}{An id variable.}
+#'   \item{species_code}{Species codes.}
+#'   \item{scientific_name}{Scientific tree names.}
+#'   \item{common_name}{Common tree names.}
+#'   \item{tree_type}{Tree types.}
+#'   \item{spp_value_assignment}{Another tree code for linking trees to benefits.}
 #'   \item{species_rating_percent}{to be determined...}
 #'   \item{basic_price_sq_in}{to be determined...}
 #'   \item{palm_trunk_cost_ft}{to be determined...}
 #'   \item{replacement_cost}{to be determined...}
 #'   \item{t_ar_sq_inches}{to be determined...}
-#'   \item{species_region}{region of tree}
+#'   \item{species_region}{Tree region.}
 #'   ...
 #' }
 #' @source \url{https://www.itreetools.org}
@@ -54,8 +54,8 @@
 #'
 #' @format A data frame with 16 rows and 11 variables:
 #' \describe{
-#'   \item{region_code}{The region code.}
-#'   \item{region_name}{The region name, meant to be human readable for users to pick the appropriate region code.}
+#'   \item{region_code}{Region code.}
+#'   \item{region_name}{Region name associated with region code. This is meant to be human readable for users to pick the appropriate region code.}
 #'   \item{electricity_kwh_to_currency}{Electricity in kwh to dollar amount.}
 #'   \item{natural_gas_kbtu_to_currency}{Natural gas in kbtu to dollar amount.}
 #'   \item{h20_gal_to_currency}{H20 in gallons to dollar amount.}
