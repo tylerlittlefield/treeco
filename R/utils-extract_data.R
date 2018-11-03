f19a6e9e75072ea0f2ab3e67214ff5a522861c27
tylurp <tylurp1@gmail.com>
2018-11-03 11:15:58 -0700

    extract_data cleanup

--- R/utils-extract_data.R
+++ R/utils-extract_data.R
@@ -1,19 +1,9 @@
 extract_data <- function(data, common_col, botanical_col, dbh_col, region, unit) {
 
-  # Might need to remove NA's first? Given the keep.rownames=TRUE?
-  ifelse(
-    test = inherits(data, "data.frame"),
-    yes = trees <- data.table::as.data.table(data, keep.rownames = TRUE),
-    no = trees <- data.table::fread(data)
-  )
-
-  # Remove NA's if an in common or botanical col
-  # discarded_records <- dim(trees)[1] - dim(trees[!with(trees, is.na(trees[[common_col]]) | is.na(trees[[botanical_col]])), ])[1]
-  #
-  # warning_message <- paste0(discarded_records, " records were discarded due to missing common or botanical names. Please use eco_guess to resolve these missing values.")
-  #
-  # if(discarded_records > 0)
-  #   warning(warning_message, call. = FALSE)
+  # If it's a dataframe, convert it to a table. Otherwise, we assume it's a csv.
+  ifelse(test = inherits(data, "data.frame"),
+         yes = trees <- data.table::as.data.table(data, keep.rownames = TRUE),
+         no = trees <- data.table::fread(data))
 
   trees <- trees[!with(trees, is.na(trees[[common_col]]) | is.na(trees[[botanical_col]])), ]
 
@@ -32,26 +22,21 @@ extract_data <- function(data, common_col, botanical_col, dbh_col, region, unit)
   trees$common_name <- as.character(trees$common_name)
   trees$dbh_val <- as.numeric(trees$dbh_val)
 
-  # Assert that the common_name is character, the dbh column is numeric, and
-  # the region parameter exists.
+  # Assert that the region parameter exists.
   stopifnot(region %in% unique(treeco::money$region_code))
 
   trees <- trees[, .SD, .SDcol = c("rn", "common_name", "botanical_name", "dbh_val")][trees$dbh_val > 0]
   benefits <- benefits[benefits$species_region == region]
   species <- species[species$species_region == region]
 
-  ifelse(
-    test = unit == "in",
-    yes = trees$dbh_val <- trees$dbh_val * 2.54,
-    no = trees
-    )
+  ifelse(test = unit == "in",
+         yes = trees$dbh_val <- trees$dbh_val * 2.54,
+         no = trees)
 
-  output <- list(
-    trees = trees,
-    benefits = benefits,
-    species = species,
-    money = money
-  )
+  output <- list(trees = trees,
+                 benefits = benefits,
+                 species = species,
+                 money = money)
 
   return(output)
 }
