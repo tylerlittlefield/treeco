16bef426b6e4e7fc38cea8068f736135c6f3ae87
tylurp <tylurp1@gmail.com>
2018-11-09 11:15:11 -0800

    perl=TRUE in gsub calls

--- R/utils-extract_matches.R
+++ R/utils-extract_matches.R
@@ -15,26 +15,27 @@ extract_matches <- function(tree_data, species_data, n) {
 
   '%nin%' <- Negate('%in%')
 
-  # Extract unique common names, convert to lower case, remove punctuation
+  # Extract unique common names, convert to lower case, remove punctuation.
+  # NOTE: perl = TRUE as suggested by Arun here: https://stackoverflow.com/questions/29273928
   unique_commons <- unique(trees[, "common_name"])
   unique_commons <- na.omit(unique_commons)
   unique_commons$common_name <- tolower(unique_commons$common_name)
-  unique_commons$common_name <- gsub('[[:punct:]]+', '', unique_commons$common_name)
+  unique_commons$common_name <- gsub('[[:punct:]]+', '', unique_commons$common_name, perl = TRUE)
   unique_commons$common_name <- trimws(unique_commons$common_name, "both") # Save for end?
 
   # Extract unique botanical names, conver to lower case, remove punctuation
   unique_botanicals <- unique(trees[, "botanical_name"])
   unique_botanicals <- na.omit(unique_botanicals)
   unique_botanicals$botanical_name <- tolower(unique_botanicals$botanical_name)
-  unique_botanicals$botanical_name <- gsub('[[:punct:]]+', '', unique_botanicals$botanical_name)
+  unique_botanicals$botanical_name <- gsub('[[:punct:]]+', '', unique_botanicals$botanical_name, perl = TRUE)
   unique_botanicals$botanical_name <- trimws(unique_botanicals$botanical_name, "both") # Save for end?
 
   species$common_name_m <- tolower(species$common_name)
-  species$common_name_m <- gsub('[[:punct:]]+', '', species$common_name_m)
+  species$common_name_m <- gsub('[[:punct:]]+', '', species$common_name_m, perl = TRUE)
   species$common_name_m <- trimws(species$common_name_m, "both") # Save for end?
 
   species$botanical_name_m <- tolower(species$scientific_name)
-  species$botanical_name_m <- gsub('[[:punct:]]+', '', species$botanical_name_m)
+  species$botanical_name_m <- gsub('[[:punct:]]+', '', species$botanical_name_m, perl = TRUE)
   species$botanical_name_m <- trimws(species$botanical_name_m, "both") # Save for end?
 
   vec <- sapply(unique_commons$common_name, function(x) which.max(string_dist(x, species$common_name_m)), USE.NAMES = FALSE)
@@ -56,11 +57,11 @@ extract_matches <- function(tree_data, species_data, n) {
   unique_botanicals_1 <- unique_botanicals[sim >= n]
 
   trees$common_name <- tolower(trees$common_name)
-  trees$common_name <- gsub('[[:punct:]]+', '', trees$common_name)
+  trees$common_name <- gsub('[[:punct:]]+', '', trees$common_name, perl = TRUE)
   trees$common_name <- trimws(trees$common_name, "both") # Save for end?
 
   trees$botanical_name <- tolower(trees$botanical_name)
-  trees$botanical_name <- gsub('[[:punct:]]+', '', trees$botanical_name)
+  trees$botanical_name <- gsub('[[:punct:]]+', '', trees$botanical_name, perl = TRUE)
   trees$botanical_name <- trimws(trees$botanical_name, "both") # Save for end?
 
   trees_common <- trees[unique_commons_1, on = "common_name"]
